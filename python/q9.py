004a0ce944588e49429dfd7be98ac644a484975e
Tianxiao Zhao <tzh@kth.se>
2017-03-15 14:51:52 +0100

    Update q9.py

--- python/q9.py
+++ python/q9.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+        # Determine whether an integer is a palindrome.
+        # type x: int
+        # rtype: bool
+        
+        # initialize
         reverse_x = 0
         old_x = x
+        
+        # rule out negative
         if x < 0:
             return False
+        
+        # get the reverse of integer
         while x != 0:
             reverse_x = 10*reverse_x + x%10
             x /= 10
+            
         return reverse_x == old_x
