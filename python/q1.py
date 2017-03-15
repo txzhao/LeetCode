582473f5d5a7a34ccb2c5a078f859060ef90afbf
Tianxiao Zhao <tzh@kth.se>
2017-03-15 12:57:59 +0100

    Update q1.py

--- python/q1.py
+++ python/q1.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-        :You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+        # You may assume that each input would have exactly one solution, and you may not use the same element twice.
         
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        # type nums: List[int]
+        # type target: int
+        # rtype: List[int]
 
+        # initialize
         diff = [target - x for x in nums]
+        
         for i in range(len(nums)):
             try:
                 ind = diff.index(nums[i])
                 if i != ind:
                     return [i, ind]
+            
+            # throw exception if value is not found in the list
             except ValueError:
                 continue
