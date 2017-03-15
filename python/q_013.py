5e034371370feff484d8ddc923fd0b7459b72710
Tianxiao Zhao <tzh@kth.se>
2017-03-15 21:44:36 +0100

    roman to integer

---
+++ python/q_013.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        # Given a roman numeral, convert it to an integer.
+        # type s: str
+        # rtype: int
+        
+        sum = 0
+        romans = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        for i in range(len(s) - 1):
+            if romans[s[i]] < romans[s[i + 1]]:
+                sum -= romans[s[i]]
+            else:
+                sum += romans[s[i]]
+        return sum + romans[s[-1]]
