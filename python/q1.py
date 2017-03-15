dc94212f8bdd8e7f2347544c926145f420879ba4
Tianxiao Zhao <tzh@kth.se>
2017-03-15 11:10:41 +0100

    Update and rename 01.py to q1.py

--- python/q1.py
+++ python/q1.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def twoSum(self, nums, target):
         """
+        :Given an array of integers, return indices of the two numbers such that they add up to a specific target.
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
