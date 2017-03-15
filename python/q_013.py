0f3191e7780198f5340dc3b9f2e084892c9c39be
Tianxiao Zhao <tzh@kth.se>
2017-03-15 21:48:12 +0100

    roman to integer

--- python/q_013.py
+++ python/q_013.py
@@ -4,11 +4,16 @@ class Solution(object):
         # type s: str
         # rtype: int
         
-        sum = 0
+        # initialize a dictionary
         romans = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        sum = 0
+        
+        # check if current roman character has smaller value than the right roman character
+        # to decide the sign of the roman
         for i in range(len(s) - 1):
             if romans[s[i]] < romans[s[i + 1]]:
                 sum -= romans[s[i]]
             else:
                 sum += romans[s[i]]
+                
         return sum + romans[s[-1]]
