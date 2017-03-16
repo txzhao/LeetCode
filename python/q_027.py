16652d9ed5afd4a4237c0b2ee4a9127dbb49677d
Tianxiao Zhao <tzh@kth.se>
2017-03-16 15:52:51 +0100

    Remove Element

---
+++ python/q_027.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        # Given an array and a value, remove all instances of that value in place and return the new length.
+        # Do not allocate extra space for another array, you must do this in place with constant memory.
+        # type nums: List[int]
+        # type val: int
+        # rtype: int
+        
+        while True:
+            try:
+                nums.remove(val)
+            except:
+                return len(nums)
