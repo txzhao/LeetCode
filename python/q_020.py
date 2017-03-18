2d5a309d8d298c09044a4dd8763df9d7d8adb534
Tianxiao Zhao <tzh@kth.se>
2017-03-18 23:08:26 +0100

    Update q_020.py

--- python/q_020.py
+++ python/q_020.py
@@ -5,13 +5,20 @@ class Solution(object):
         # type s: str
         # rtype: bool
         
+        # initialize a dictionary with brackets included as keys and values
+        # define a stack 'brackets'
         dict = {')':'(','}':'{',']':'['}
         brackets = []
         
         for char in s:
+            # append left brackets into stack
             if char in dict.values():
                 brackets.append(char)
+            
+            # check if first right bracket fits the last left one
+            # right bracket always starts to appear after the last left one
             elif char in dict.keys():
+                # check if the string starts with a right bracket
                 if brackets == [] or dict[char] != brackets.pop():
                     return False
             else:
