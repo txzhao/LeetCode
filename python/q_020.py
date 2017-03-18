7a28052a3e95c0002b07a63a0380ac5544d16126
Tianxiao Zhao <tzh@kth.se>
2017-03-18 23:01:44 +0100

    Update q_020.py

--- python/q_020.py
+++ python/q_020.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        # The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+        # type s: str
+        # rtype: bool
         
         dict = {')':'(','}':'{',']':'['}
         brackets = []
