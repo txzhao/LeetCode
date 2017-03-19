33fa267b1942a45dd3b8f3890af9c0c346d2f607
Tianxiao Zhao <tzh@kth.se>
2017-03-19 21:01:27 +0100

    reverse string

---
+++ python/q_344.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def reverseString(self, s):
+        # Write a function that takes a string as input and returns the string reversed.
+        # type s: str
+        # rtype: str
+        
+        return s[::-1]
