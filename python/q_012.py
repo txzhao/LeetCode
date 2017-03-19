cf7b1657b2c56e0f99dafc88d486d1c21865c76c
Tianxiao Zhao <tzh@kth.se>
2017-03-19 11:34:50 +0100

    integer to roman

--- python/q_012.py
+++ python/q_012.py
@@ -1,16 +1,18 @@
 class Solution(object):
     def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
+        # Given an integer, convert it to a roman numeral.
+        # Input is guaranteed to be within the range from 1 to 3999.
+        # type num: int
+        # rtype: str
         
+        # define tuples which contain int and corresponding roman
         int_rom = [(1, 'I'), (4, 'IV'), (5, 'V'), (9, 'IX'), (10, 'X'), (40, 'XL'), (50, 'L'), 
             (90, 'XC'), (100, 'C'), (400, 'CD'), (500, 'D'), (900, 'CM'), (1000, 'M')]
+        rightpoint = len(int_rom) - 1
         romans = ''
-        rightpoint = 12
- 
-        while num != 0:
+        
+        # transform the int into romans
+        while num:
             if num >= int_rom[rightpoint][0]:
                 romans += int_rom[rightpoint][1]
                 num -= int_rom[rightpoint][0]
