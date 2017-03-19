d874ec951f1b715e01957e88da5be601375745b7
Tianxiao Zhao <tzh@kth.se>
2017-03-19 12:10:41 +0100

    Update q_035.py

--- python/q_035.py
+++ python/q_035.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        #
+        # Given a sorted array and a target value, return the index if the target is found. 
+        # If not, return the index where it would be if it were inserted in order.
         # type nums: List[int]
         # type target: int
         # rtype: int
@@ -9,5 +10,6 @@ class Solution(object):
             if target <= value:
                 return idx
             else:
+                # check if 
                 if idx >= len(nums) - 1:
                     return idx + 1
