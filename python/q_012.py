3a2a8959cb5cff86e986cb0096ee7792e79cc1fd
Tianxiao Zhao <tzh@kth.se>
2017-03-19 11:40:05 +0100

    Integer to Roman

--- python/q_012.py
+++ python/q_012.py
@@ -26,5 +26,5 @@ class Solution(object):
         # C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
         # X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
         # I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-        # return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        # return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]; 
         
