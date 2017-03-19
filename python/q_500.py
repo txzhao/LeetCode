93499f8dfed6a9e2d6b045ea8783ea90d696fcb6
Tianxiao Zhao <tzh@kth.se>
2017-03-19 19:09:45 +0100

    keyboard row

--- python/q_500.py
+++ python/q_500.py
@@ -1,9 +1,9 @@
 class Solution(object):
     def findWords(self, words):
-        """
-        :type words: List[str]
-        :rtype: List[str]
-        """
+        # Given a List of words, return the words that can be typed 
+        # using letters of alphabet on only one row's of American keyboard.
+        # type words: List[str]
+        # rtype: List[str]
         
         line1, line2, line3 = set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm')
         chosen_w = []
