e21f38bb1e62ac94695df35cdf393af1390571d3
Tianxiao Zhao <tzh@kth.se>
2017-03-19 17:44:31 +0100

    Create q_461.py

---
+++ python/q_461.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        # Given two integers x and y, calculate the Hamming distance.
+        # The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+        # type x: int
+        # type y: int
+        # rtype: int
+        
+        return len((bin(x^y)[2:]).replace('0', ''))
