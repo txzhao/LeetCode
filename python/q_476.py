30392347fc73ed60db92cf8a9520b22c1792a303
Tianxiao Zhao <tzh@kth.se>
2017-03-19 18:44:39 +0100

    number complement

--- python/q_476.py
+++ python/q_476.py
@@ -4,8 +4,11 @@ class Solution(object):
         # The complement strategy is to flip the bits of its binary representation.
         # type num: int
         # rtype: int
-
+        
+        # define a binary number with all bits equal to 1
         n = len(bin(num)[2:])
         ones = 2**n - 1
-
+        
+        # use XOR operation to find complement
         return int(bin(num^ones), 2)
+    
