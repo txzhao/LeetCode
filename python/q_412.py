498de0be70388548cf96dfdaddb2732f729172be
Tianxiao Zhao <tzh@kth.se>
2017-03-19 20:45:02 +0100

    Fizz Buzz

--- python/q_412.py
+++ python/q_412.py
@@ -19,3 +19,6 @@ class Solution(object):
             outs.append(out)
         
         return(outs)
+    
+        ## more elegent and clearer one
+        # return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
