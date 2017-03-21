47a65082a8a621482b5529b0e2b910386ecebcd3
Tianxiao Zhao <tzh@kth.se>
2017-03-21 16:21:01 +0100

    single number

--- python/q_136.py
+++ python/q_136.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        # Given an array of integers, every element appears twice except for one. Find that single one.
+        # type nums: List[int]
+        # rtype: int
         
         return sum(list(set(nums)))*2 - sum(nums)
