be03070e9bfe42718b05239b8120354827ce3f06
Tianxiao Zhao <tzh@kth.se>
2017-03-22 16:04:07 +0100

    add digits

--- python/q_258.py
+++ python/q_258.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def addDigits(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        while(num >= 10):
+        # Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+        # type num: int
+        # rtype: int
+        
+        # check if sum of digits needs another sum operation of digits
+        while num >= 10:
             temp = 0
-            while(num > 0):
+            while num > 0:
                 temp += num % 10
                 num /= 10
             num = temp
