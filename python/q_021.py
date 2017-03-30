649e04d39a2b2ad02e7596736f5463f0282b0232
Tianxiao Zhao <tzh@kth.se>
2017-03-30 14:22:49 +0200

    merge two sorted lists

--- python/q_021.py
+++ python/q_021.py
@@ -6,11 +6,13 @@
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+        # Merge two sorted linked lists and return it as a new list. 
+        # The new list should be made by splicing together the nodes of the first two lists.
+        # type l1: ListNode
+        # type l2: ListNode
+        # rtype: ListNode
+        
+        # define a linked list
         root = node = ListNode(0)
         
         while l1 or l2:
