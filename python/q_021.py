1cc6b9475035356ecde37eb9645781a9980350bf
Tianxiao Zhao <tzh@kth.se>
2017-03-30 14:17:57 +0200

    Create q_021.py

---
+++ python/q_021.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        root = node = ListNode(0)
+        
+        while l1 or l2:
+            if not l1:
+                node.next = l2
+                return root.next
+            if not l2:
+                node.next = l1
+                return root.next
+            if l1.val <= l2.val:
+                node.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                node.next = ListNode(l2.val)
+                l2 = l2.next
+            node = node.next
